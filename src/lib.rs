d4c32c0f650f4daa8186ea031798b02e9feba791
blue-orange-yellow <immoraroutine@gmail.com>
2026-03-23 23:41:17 +0900

    feat: add project skeleton with Cargo.toml and module stubs

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod propagation;
+pub mod semantic;
+pub mod producer;
+pub mod consumer;
+
+// pub use producer::TracingProducer;
+// pub use consumer::TracingConsumer;
+// pub use consumer::extract_context_from_message;
